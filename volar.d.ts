62798bc34424a9c6fe86afea09823162664dccac
xiaoxian521 <1923740402@qq.com>
2022-06-24 11:36:21 +0800

    fix: volar

--- volar.d.ts
+++ volar.d.ts
@@ -1,7 +1,6 @@
 // Auto generated component declarations
 declare module "vue" {
   export interface GlobalComponents {
-    // @ts-ignore
     PureDescriptions: typeof import("@pureadmin/descriptions")["PureDescriptions"];
   }
 }
